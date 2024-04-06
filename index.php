30943a461858ad50238f7596f565138130b36164
Ahmed Ihab <ahmedinouh@gmail.com>
2024-04-06 22:42:52 +0200

    update

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shout it!</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+    <div id="container">
+    <header>
+        <h1>SHOUT IT! shoutbox</h1>
+    </header>
+        <div id="shouts">
+            <ul>
+            <li class="shout"><span>10:15 PM - </span></li>
+            <li class="shout"><span>10:15 PM - </span></li>
+            <li class="shout"><span>10:15 PM - </span></li>
+            <li class="shout"><span>10:15 PM</span></li>
+            <li class="shout"><span>10:15 PM</span></li>
+            <li class="shout"><span>10:15 PM</span></li>
+
+            </ul>
+        </div>
+
+    </div>
+    
+</body>
+</html>
